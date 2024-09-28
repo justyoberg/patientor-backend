54b5af41468a576de913e865eb9a4e6280d2f98f
Justy <justyoberg1@gmail.com>
2024-09-28 16:38:46 -0700

    Create service for returning NonSensitivePatients

--- src/routes/patients.ts
+++ src/routes/patients.ts
@@ -1,10 +1,11 @@
-import express from 'express';
+import express, { Response } from 'express';
 import patientsService from '../services/patientsService';
+import { NonSensitivePatient } from '../types';
 
 const router = express.Router();
 
-router.get('/', (_req, res) => {
-  res.send(patientsService.getPatients());
+router.get('/', (_req, res: Response<NonSensitivePatient[]>) => {
+  res.send(patientsService.getNonSensitivePatients());
 });
 
 export default router;
--- src/services/patientsService.ts
+++ src/services/patientsService.ts
@@ -1,10 +1,21 @@
-import { Patient } from '../types';
+import { NonSensitivePatient, Patient } from '../types';
 import patients from '../../data/patients';
 
 const getPatients = (): Patient[] => {
   return patients;
 };
 
+const getNonSensitivePatients = (): NonSensitivePatient[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
 export default {
   getPatients,
+  getNonSensitivePatients,
 };
